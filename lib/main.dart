1167d62a9b7e44e3ba4ce26eebf98ee2765ac405
Jay <jayandratodawat@gmail.com>
2024-01-15 18:04:39 +0530

    Project Updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,8 @@
 import 'package:ev_charging_point_app/features/presentation/providers/auth_checker.dart';
-import 'package:ev_charging_point_app/features/presentation/screens/login_screen.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-import 'package:hive_flutter/adapters.dart';
-import 'package:path_provider/path_provider.dart';
-
-import 'features/data/models/station_model.dart';
 import 'firebase_options.dart';
 
 void main() async {
@@ -18,9 +13,6 @@ void main() async {
       options: DefaultFirebaseOptions.currentPlatform
   );
 
-  // await Hive.initFlutter((await getApplicationDocumentsDirectory()).path);
-  // Hive.registerAdapter(StationModelAdapter());
-  // await Hive.openBox<StationModel>('stations');
 
   runApp(const ProviderScope(child:MyApp()));
 }
@@ -31,7 +23,7 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'EV Charging Point',
       home: AuthChecker(),
